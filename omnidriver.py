715dc43387b7f828097456bee8b00cfeae702b5e
gal arbel <84223483+galarb@users.noreply.github.com>
2025-04-18 21:44:38 +0300

    RPI version

--- omnidriver.py
+++ omnidriver.py
@@ -1,51 +1,41 @@
-from machine import Pin, PWM, TouchPad
-import neopixel
-from time import sleep, ticks_ms
-import math
-import random
-recorded_values = []
-recorded_valuesproc = []
-
+import RPi.GPIO as GPIO
+from time import sleep
 
 class omnidriver:
     def __init__(self, in1, in2):
-        self.in1 = Pin(in1, Pin.OUT)
-        self.in2 = Pin(in2, Pin.OUT)
-        
-        self.pwm1 = PWM(self.in1)
-        self.pwm2 = PWM(self.in2)
-       
-        self.pwm1.freq(500)
-        self.pwm2.freq(500)
-        
-        #stop the motor hard
-        self.pwm1.duty(1023)
-        self.pwm2.duty(1023)
-        
+        self.in1 = in1
+        self.in2 = in2
 
+        GPIO.setmode(GPIO.BCM)
+        #GPIO.cleanup()
 
-    
+        GPIO.setup(self.in1, GPIO.OUT)
+        GPIO.setup(self.in2, GPIO.OUT)
+        
+        self.pwm1 = GPIO.PWM(self.in1, 500)  # 500 Hz
+        self.pwm2 = GPIO.PWM(self.in2, 500)
+        
+        self.pwm1.start(100)  # brake
+        self.pwm2.start(100)
+
 
     def motgo(self, speed):
-        pwm_value = int(min(max(abs(speed), 0), 100) * 10.23)  # Map -100 to 100 to 0 to 1023
-        #print('speed pwm value = ', pwm_value)
+        pwm_value = min(max(abs(speed), 0), 100)
         if speed > 0:
-            # Forward direction
-            self.pwm1.duty(pwm_value)
-            self.pwm2.duty(0)
+            self.pwm1.ChangeDutyCycle(pwm_value)
+            self.pwm2.ChangeDutyCycle(0)
         elif speed < 0:
-            # Reverse direction
-            self.pwm1.duty(0)
-            self.pwm2.duty(pwm_value)
+            self.pwm1.ChangeDutyCycle(0)
+            self.pwm2.ChangeDutyCycle(pwm_value)
         else:
-            # Stop the motor
-            self.pwm1.duty(0)
-            self.pwm2.duty(0)    
+            self.pwm1.ChangeDutyCycle(0)
+            self.pwm2.ChangeDutyCycle(0)
+        print(f"PWM1: {pwm_value if speed > 0 else 0}\nPWM2: {pwm_value if speed < 0 else 0}")
 
-    
     def stophard(self):
-            self.pwm1.duty(1023)
-            self.pwm2.duty(1023)
-            
+        self.pwm1.ChangeDutyCycle(100)
+        self.pwm2.ChangeDutyCycle(100)
+        sleep(0.01)
 
-    
+
+
