b04f92edff4b536d9eec0310b8846f034468ae09
gal arbel <84223483+galarb@users.noreply.github.com>
2025-05-02 09:05:32 +0300

    gpiozero

--- omnidriver.py
+++ omnidriver.py
@@ -1,40 +1,32 @@
-import RPi.GPIO as GPIO
+from gpiozero import PWMOutputDevice, DigitalOutputDevice
 from time import sleep
 
 class omnidriver:
     def __init__(self, in1, in2):
-        self.in1 = in1
-        self.in2 = in2
-
-        GPIO.setmode(GPIO.BCM)
-        #GPIO.cleanup()
-
-        GPIO.setup(self.in1, GPIO.OUT)
-        GPIO.setup(self.in2, GPIO.OUT)
+        self.in1 = PWMOutputDevice(in1)
+        self.in2 = PWMOutputDevice(in2)
         
-        self.pwm1 = GPIO.PWM(self.in1, 500)  # 500 Hz
-        self.pwm2 = GPIO.PWM(self.in2, 500)
-        
-        self.pwm1.start(100)  # brake
-        self.pwm2.start(100)
+        self.in1.value = 0  # brake
+        self.in2.value = 0
 
 
     def motgo(self, speed):
-        pwm_value = min(max(abs(speed), 0), 100)
+        pwm_value = min(max(abs(speed) / 100.0, 0.0), 1.0)#convert to%
         if speed > 0:
-            self.pwm1.ChangeDutyCycle(pwm_value)
-            self.pwm2.ChangeDutyCycle(0)
+            self.in1.value = pwm_value
+            self.in2.value = 0
         elif speed < 0:
-            self.pwm1.ChangeDutyCycle(0)
-            self.pwm2.ChangeDutyCycle(pwm_value)
+            self.in1.value = 0
+            self.in2.value = pwm_value
+            
         else:
-            self.pwm1.ChangeDutyCycle(0)
-            self.pwm2.ChangeDutyCycle(0)
-        print(f"PWM1: {pwm_value if speed > 0 else 0}\nPWM2: {pwm_value if speed < 0 else 0}")
+            self.in1.value = 0
+            self.in2.value = 0
+        #print(f"PWM1: {pwm_value if speed > 0 else 0}\nPWM2: {pwm_value if speed < 0 else 0}")
 
     def stophard(self):
-        self.pwm1.ChangeDutyCycle(100)
-        self.pwm2.ChangeDutyCycle(100)
+        self.in1.value = 1
+        self.in2.value = 1
+            
         sleep(0.01)
-
 
