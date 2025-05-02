36f3fa321388b4a46f2890d19d1cd00f3afd8cac
gal arbel <84223483+galarb@users.noreply.github.com>
2025-05-02 09:06:46 +0300

    gpiozero

--- scorerrobot.py
+++ scorerrobot.py
@@ -1,32 +1,26 @@
 from scorer import scorer
 from time import sleep
-import RPi.GPIO as GPIO
+from signal import pause
 
-#DOA, D1A, D2A, D3A, DOB, D1B, D2B, D3B, in1_pin, in2_pin, in3_pin, in4_pin, drib1_pin, drib2_pin, RL1_pin, RL2_pin, RL3_pin
+#DOA, D1A, D2A, D3A, DOB, D1B, D2B, D3B, in1_pin, in2_pin, in3_pin, in4_pin, drib1_pin, drib2_pin, RL1_pin, RL2_pin, RL3_pin, button _pin
 
-pilot = scorer(10, 22, 17, 27, 5, 6, 19, 13, 21, 20, 16, 12, 8, 7, 24, 23, 18)
+pilot = scorer(10, 22, 17, 27, 5, 6, 19, 13, 21, 20, 16, 12, 8, 7, 24, 23, 18, 4)
+pause()  # Keeps the script running to wait for button presses
 
-#pilot.dribble(-50)
-for _ in range(10):
-    #pilot.govector(10, 10, 10)
-    #pilot.gomotor(0, 50)
-    #pilot.kick()
-    #pilot.dribble(50)
-    sleep(2)
-    #pilot.govector(10, -10, -10)
-    sleep(2)
-    #pilot.govector(0, 0, 30)
-    sleep(2)
-    #pilot.govector(0, 0, -30)
-    sleep(2)
 
-pilot.govector(0, 0, 0)
+try:
+    while True:
+        if pilot.modeflag == 0:
+            # IDLE MODE
+            pilot.govector(0, 0, 0)  # No movement
+            pilot.dribble(0)         # Maybe small dribble? Or completely off.
+        else:
+            # HUNT MODE
+            pilot.govector(0, 0, 20)  # Example: Go forward hunting
+            pilot.dribble(0)
 
-#pilot.govector(0, 0, 0)
+        sleep(0.1)  # Small sleep to avoid high CPU usage
 
-#pilot.govector(0, 0, 0)
-#test2.motgo(0)
-#test3.motgo(0)
-#test4.motgo(0)
-print('stopped')
-GPIO.cleanup()
+except KeyboardInterrupt:
+    print('Stopped by user')
+    pilot.stophard()
