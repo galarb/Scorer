671aee73dfd2bd5fe731a6f9c17503207e6c29f3
gal arbel <84223483+galarb@users.noreply.github.com>
2025-08-29 20:34:32 +0300

    PCB compatible

--- scorerrobot.py
+++ scorerrobot.py
@@ -4,19 +4,17 @@ from threading import Thread
 from signal import pause
 from gpiozero import Device
 from gpiozero.pins.lgpio import LGPIOFactory
-from rpiuart import RpiUart
 
 
 # Initialize scorer 
 pilot = scorer(
-    motorA_pins=(10, 22, 17, 27),
-    motorB_pins=(5, 6, 19, 13),
+    motorA_pins=(22, 10, 17, 27),
+    motorB_pins=(6, 5, 19, 13),
     kickerdribbler_pins=(16, 12, 20, 21),
-    sensor_pins=(24, 23, 18),
+    sensor_pins=(24, 18, 23),
     modebutton_pin=4
 )
-uart = RpiUart()
-uart.start()
+
 
 # Worker thread function
 def robot_loop():
@@ -30,26 +28,15 @@ def robot_loop():
                 pilot.kickermotor.motgo(0)
                 if pilot.ball_loaded():
                     print('ball ready')
-                sleep(1)
+                sleep(0.1)
 
             elif mode == 1:  # HUNT mode
-                msg = uart.get_message()
-                if msg:
-                    if msg == "None":
-                        print("No ball detected")
-                    else:
-                        try:
-                            x, y, area = map(int, msg.split(','))
-                            print(f"Ball: x={x}, y={y}, area={area}")
-                        except ValueError:
-                            print(f"Bad data: {msg}")
-                sleep(0.01)
-               
+                if(not pilot.check_cam()):
+                    pilot.govector(0, 0, 0)
+                #pilot.scan()
+                sleep(0.12)
 
             elif mode == 2:  # MANUAL mode
-                #print("Manual behavior running...")
-
-                # Manual control handler, probably from mobile
                 # pilot.update_from_manual_commands()  ← to be implemented
                 pass
 
